77469dd14d88dff9f9ee216259caa6ea96039025
Logan Larson <loganlarson7@icloud.com>
2021-03-21 07:55:06 -0500

    Update wakeup.js

--- wakeup.js
+++ wakeup.js
@@ -4,59 +4,59 @@ const open = require("open");
 const he = require("he");
 
 var options = {
-  attributeNamePrefix : "",
+  attributeNamePrefix: "",
   attrNodeName: "attr", //default is 'false'
-  textNodeName : "#text",
-  ignoreAttributes : false,
-  ignoreNameSpace : false,
-  allowBooleanAttributes : false,
-  parseNodeValue : true,
-  parseAttributeValue : false,
+  textNodeName: "#text",
+  ignoreAttributes: false,
+  ignoreNameSpace: false,
+  allowBooleanAttributes: false,
+  parseNodeValue: true,
+  parseAttributeValue: false,
   trimValues: true,
   cdataTagName: "__cdata", //default is 'false'
   cdataPositionChar: "\\c",
   parseTrueNumberOnly: false,
   arrayMode: false, //"strict"
-  attrValueProcessor: (val, attrName) => he.decode(val, {isAttributeValue: true}),//default is a=>a
-  tagValueProcessor : (val, tagName) => he.decode(val), //default is a=>a
+  attrValueProcessor: (val, attrName) => he.decode(val, { isAttributeValue: true }),//default is a=>a
+  tagValueProcessor: (val, tagName) => he.decode(val), //default is a=>a
   stopNodes: ["parse-me-as-string"]
 };
 
 function getWakeUp() {
 
   return fetch("https://feeds.npr.org/510318/podcast.xml")
-      .then(response => response.text())
-      .then(data => {
-        
-        var json = parser.parse(data, options);
+    .then(response => response.text())
+    .then(data => {
 
-        var item = json.rss.channel.item[0];
-        var link = item.enclosure.attr.url;
+      var json = parser.parse(data, options);
 
-        console.log(link);
+      var item = json.rss.channel.item[0];
+      var link = item.enclosure.attr.url;
 
-        open(link, {app: 'google chrome'});
+      console.log(link);
 
-      })
+      open(link, { app: 'google chrome' });
+
+    })
 }
 
 function getHowIBuiltThis() {
 
   return fetch("https://feeds.npr.org/510313/podcast.xml")
-      .then(response => response.text())
-      .then(data => {
-        
-        var json = parser.parse(data, options);
+    .then(response => response.text())
+    .then(data => {
 
-        var item = json.rss.channel.item[0];
-        var link = item.enclosure.attr.url;
+      var json = parser.parse(data, options);
 
-        console.log(link);
+      var item = json.rss.channel.item[0];
+      var link = item.enclosure.attr.url;
 
-        open(link);
+      console.log(link);
 
-      })
+      open(link);
+
+    })
 }
 
-getHowIBuiltThis();
-//getWakeUp();
+//getHowIBuiltThis();
+getWakeUp();
